2c0a945c86945e2e5a3c17f86488cd1625b13e07
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-07-23 16:35:54 +0200

    ref(sdk-updates-broadcast): Update design & add raven alert

--- static/app/components/sidebar/broadcastSdkUpdates.tsx
+++ static/app/components/sidebar/broadcastSdkUpdates.tsx
@@ -2,13 +2,16 @@ import styled from '@emotion/styled';
 import groupBy from 'lodash/groupBy';
 
 import ProjectBadge from 'app/components/idBadge/projectBadge';
-import {t} from 'app/locale';
+import Tag from 'app/components/tag';
+import {IconWarning} from 'app/icons/iconWarning';
+import {t, tct, tn} from 'app/locale';
 import space from 'app/styles/space';
 import {Project, ProjectSdkUpdates, SDKUpdatesSuggestion} from 'app/types';
 import getSdkUpdateSuggestion from 'app/utils/getSdkUpdateSuggestion';
 import withProjects from 'app/utils/withProjects';
 import withSdkUpdates from 'app/utils/withSdkUpdates';
 
+import Alert from '../alert';
 import Collapsible from '../collapsible';
 import List from '../list';
 import ListItem from '../list/listItem';
@@ -26,15 +29,21 @@ const flattenSuggestions = (list: ProjectSdkUpdates[]) =>
     []
   );
 
-const BroadcastSdkUpdates = ({projects, sdkUpdates}: Props) => {
+function BroadcastSdkUpdates({projects, sdkUpdates}: Props) {
   if (!sdkUpdates) {
     return null;
   }
 
   // Are there any updates?
-  if (flattenSuggestions(sdkUpdates).length === 0) {
+  if (!flattenSuggestions(sdkUpdates).length) {
     return null;
   }
+
+  const deprecatedRavenSdkUpdates = sdkUpdates.filter(
+    sdkUpdate =>
+      sdkUpdate.sdkName.includes('raven') &&
+      sdkUpdate.suggestions.some(suggestion => suggestion.type === 'changeSdk')
+  );
 
   // Group SDK updates by project
   const items = Object.entries(groupBy(sdkUpdates, 'projectId'));
@@ -47,50 +56,73 @@ const BroadcastSdkUpdates = ({projects, sdkUpdates}: Props) => {
         'We recommend updating the following SDKs to make sure you’re getting all the data you need.'
       )}
     >
-      <UpdatesList>
-        <Collapsible>
-          {items.map(([projectId, updates]) => {
-            const project = projects.find(p => p.id === projectId);
-            if (project === undefined) {
-              return null;
+      {!!deprecatedRavenSdkUpdates.length && (
+        <StyledAlert type="warning" icon={<IconWarning />}>
+          {tct(
+            `[first-sentence]. Any SDK that has the package name ‘raven’ may be missing data. Migrate to the latest SDK version.`,
+            {
+              ['first-sentence']: tn(
+                'You have %s project using a deprecated version of the Sentry client',
+                'You have %s projects using a deprecated version of the Sentry client',
+                deprecatedRavenSdkUpdates.length
+              ),
             }
+          )}
+        </StyledAlert>
+      )}
+      <UpdatesList>
+        <Collapsible maxVisibleItems={5}>
+          {items
+            .map(([projectId, updates]) => {
+              const project = projects.find(p => p.id === projectId);
+              if (!project) {
+                return null;
+              }
 
-            return (
-              <div key={project.id}>
-                <SdkProjectBadge project={project} />
-                <Suggestions>
-                  {updates.map(sdkUpdate => (
-                    <div key={sdkUpdate.sdkName}>
-                      <SdkName>
-                        {sdkUpdate.sdkName}{' '}
-                        <SdkOutdatedVersion>@v{sdkUpdate.sdkVersion}</SdkOutdatedVersion>
-                      </SdkName>
-                      <List>
-                        {sdkUpdate.suggestions.map((suggestion, i) => (
-                          <ListItem key={i}>
-                            {getSdkUpdateSuggestion({
-                              sdk: {
-                                name: sdkUpdate.sdkName,
-                                version: sdkUpdate.sdkVersion,
-                              },
-                              suggestion,
-                              shortStyle: true,
-                              capitalized: true,
-                            })}
-                          </ListItem>
-                        ))}
-                      </List>
-                    </div>
-                  ))}
-                </Suggestions>
-              </div>
-            );
-          })}
+              return updates.map(({sdkName, sdkVersion, suggestions}) => {
+                const isDeprecated = suggestions.some(
+                  suggestion => suggestion.type === 'changeSdk'
+                );
+                return (
+                  <div key={sdkName}>
+                    <Header>
+                      <SdkProjectBadge project={project} />
+                      {isDeprecated && <Tag type="warning">{t('Deprecated')}</Tag>}
+                    </Header>
+                    <SdkOutdatedVersion>
+                      {tct('This project is on [current-version]', {
+                        ['current-version']: (
+                          <OutdatedVersion>{`${sdkName}@v${sdkVersion}`}</OutdatedVersion>
+                        ),
+                      })}
+                    </SdkOutdatedVersion>
+                    <StyledList>
+                      {suggestions.map((suggestion, i) => (
+                        <ListItem key={i}>
+                          {getSdkUpdateSuggestion({
+                            sdk: {
+                              name: sdkName,
+                              version: sdkVersion,
+                            },
+                            suggestion,
+                            shortStyle: true,
+                            capitalized: true,
+                          })}
+                        </ListItem>
+                      ))}
+                    </StyledList>
+                  </div>
+                );
+              });
+            })
+            .filter(item => !!item)}
         </Collapsible>
       </UpdatesList>
     </SidebarPanelItem>
   );
-};
+}
+
+export default withSdkUpdates(withProjects(BroadcastSdkUpdates));
 
 const UpdatesList = styled('div')`
   margin-top: ${space(3)};
@@ -99,24 +131,34 @@ const UpdatesList = styled('div')`
   grid-gap: ${space(3)};
 `;
 
-const Suggestions = styled('div')`
-  margin-left: calc(${space(4)} + ${space(0.25)});
+const Header = styled('div')`
   display: grid;
-  grid-auto-flow: row;
+  grid-template-columns: 1fr auto;
   grid-gap: ${space(0.5)};
+  margin-bottom: ${space(0.25)};
+  align-items: center;
+`;
+
+const SdkOutdatedVersion = styled('div')`
+  /* 24px + 8px to be aligned with the SdkProjectBadge data */
+  padding-left: calc(24px + ${space(1)});
+`;
+
+const OutdatedVersion = styled('span')`
+  color: ${p => p.theme.gray400};
 `;
 
 const SdkProjectBadge = styled(ProjectBadge)`
-  font-size: ${p => p.theme.fontSizeLarge};
+  font-size: ${p => p.theme.fontSizeExtraLarge};
+  line-height: 1;
 `;
 
-const SdkName = styled('div')`
-  font-family: ${p => p.theme.text.familyMono};
-  font-weight: bold;
+const StyledAlert = styled(Alert)`
+  margin-top: ${space(2)};
 `;
 
-const SdkOutdatedVersion = styled('span')`
-  color: ${p => p.theme.subText};
+const StyledList = styled(List)`
+  /* 24px + 8px to be aligned with the project name
+  * displayed by the SdkProjectBadge component */
+  padding-left: calc(24px + ${space(1)});
 `;
-
-export default withSdkUpdates(withProjects(BroadcastSdkUpdates));
