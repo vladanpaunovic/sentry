b49d8ed386a6d35f2326752765c36827cc0b7f26
Joshua Li <joshuarli98@gmail.com>
2021-07-23 12:19:11 -0700

    great success

--- test-x-django-pickles.py
+++ test-x-django-pickles.py
@@ -15,21 +15,8 @@ class Lol(TestCase):
         with open(f"project-{django_version}.pickle", "wb") as f:
             f.write(pickle.dumps(self.project))
 
-    def test_20_unpickles_111(self):
-        assert django_version == "2.0"
-        with open("project-1.11.pickle", "rb") as f:
-            project_11 = pickle.loads(f.read())
-
-        assert project_11 == self.project
-        assert project_11.name == self.project.name
-        assert project_11.slug == self.project.slug
-        assert project_11.organization == self.project.organization
-        assert project_11.status == self.project.status
-        assert project_11.flags == self.project.flags
-        assert project_11.platform == self.project.platform
-
-    def test_111_unpickles_20(self):
-        assert django_version == "1.11"
+    def test_21_unpickles_20(self):
+        assert django_version == "2.1"
         with open("project-2.0.pickle", "rb") as f:
             project_20 = pickle.loads(f.read())
 
@@ -40,3 +27,16 @@ class Lol(TestCase):
         assert project_20.status == self.project.status
         assert project_20.flags == self.project.flags
         assert project_20.platform == self.project.platform
+
+    def test_20_unpickles_21(self):
+        assert django_version == "2.0"
+        with open("project-2.1.pickle", "rb") as f:
+            project_21 = pickle.loads(f.read())
+
+        assert project_21 == self.project
+        assert project_21.name == self.project.name
+        assert project_21.slug == self.project.slug
+        assert project_21.organization == self.project.organization
+        assert project_21.status == self.project.status
+        assert project_21.flags == self.project.flags
+        assert project_21.platform == self.project.platform
