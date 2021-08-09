02ec7f7dd4773df83c8d9c147d1e3b7a6add5fb8
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-08-09 12:17:22 -0400

    add migration

---
+++ src/sentry/models/groupgithubfeed.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+from django.db import models
+from django.utils import timezone
+
+from sentry.db.models import (
+    BoundedBigIntegerField,
+    BoundedPositiveIntegerField,
+    FlexibleForeignKey,
+    Model,
+    sane_repr,
+)
+
+
+class FeedType(Enum):
+    BRANCH = 1
+    PULL_REQUEST = 2
+
+
+class FeedStatus(Enum):
+    DRAFT = 1
+    OPEN = 2
+    MERGED = 3
+    CLOSED = 4
+
+
+class GroupGithubFeed(Model):
+    __include_in_export__ = False
+
+    organization_id = BoundedPositiveIntegerField(db_index=True)
+    group_id = BoundedBigIntegerField()
+    branch_name = models.CharField(max_length=350)
+    feed_type = models.IntegerField()
+    feed_status = models.IntegerField(null=True)
+    author = FlexibleForeignKey("sentry.CommitAuthor", null=True)
+    display_name = models.CharField(max_length=350)
+    url = models.URLField(null=True, blank=True)
+    date_added = models.DateTimeField(default=timezone.now)
+    visible = models.BooleanField(default=True)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_groupgithubfeed"
+        unique_together = ("group_id", "branch_name")
+
+    __repr__ = sane_repr("feed_type", "group_id")
